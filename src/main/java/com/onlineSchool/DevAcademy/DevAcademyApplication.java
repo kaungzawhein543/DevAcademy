5a091c541d883fcf5a01aec047fb262f1fe2d73b
Kaung Zaw Hein <kzheindev789@gmail.com>
2026-05-15 16:04:33 +0630

    first commit

---
+++ src/main/java/com/onlineSchool/DevAcademy/DevAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.onlineSchool.DevAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlineSchool/DevAcademy/DevAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineSchool.DevAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
