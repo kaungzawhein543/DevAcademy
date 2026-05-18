d64b9a9df62b53dded6f8f58690a448fc2b22617
Kaung Zaw Hein <kzheindev789@gmail.com>
2026-05-18 14:40:55 +0630

    Implementation: remove unnecessary imports

--- src/main/java/com/onlineSchool/DevAcademy/modules/test/controller/v1/TestController.java
+++ src/main/java/com/onlineSchool/DevAcademy/modules/test/controller/v1/TestController.java
@@ -3,7 +3,6 @@ package com.onlineSchool.DevAcademy.modules.test.controller.v1;
 import com.onlineSchool.DevAcademy.common.constants.ConstantsVariables;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
